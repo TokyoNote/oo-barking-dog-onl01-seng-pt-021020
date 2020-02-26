663e30db1bfb4ca0ac5e0ad23ececcbb87708e30
Camiyah Goins <tokyonote9@gmail.com>
2020-02-26 12:13:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  
+def name
+  @this_dogs_name
+  end
+end
+
+def bark
+  puts "woof!"
+end
+
+fido = Dog.new
+fido.name= "Fido"
+
+fido.name
+
+fido.bark
+
